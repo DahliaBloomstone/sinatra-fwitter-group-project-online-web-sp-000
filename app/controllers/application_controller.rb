6cb29122434044dd4ba27880fd23c50a7f3919a0
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-28 13:21:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      @current_user = User.find_by(session[:id]) if session[:id]
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
   end
 end
