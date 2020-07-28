c755430aa801ba0f7b7782c6fafe8323e7bfa182
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-28 13:38:10 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,73 +1,21 @@
 class TweetsController < ApplicationController
-
-  get '/tweets' do
-    @tweets = Tweet.all
-    if logged_in?
-      @user = current_user
-      erb :"tweets"
-    else
-      redirect to '/login'
-    end
-  end
-
-  get '/tweets/new' do
-    if logged_in?
-      erb :"tweets/new"
-    else
-      redirect to '/login'
-    end
-  end
-
-  post '/tweets' do
-     if logged_in?
-       if params[:content] != ""
-         user = current_user
-         tweet = user.tweets.build(content: params[:content])
-       user.save
-       redirect "/tweets/#{tweet.id}"
-     else
-       redirect '/tweets/new'
-     end
+ get '/tweets' do
+   if logged_in?
+     @tweets = Tweet.all
+     erb :'tweets/tweets'
    else
-     redirect '/login'
+     redirect to '/login'
    end
  end
 
-  get '/tweets/:id' do
-    if logged_in?
-      @tweet = Tweet.find(params[:id])
-      erb :"tweets/show"
-    else
-      redirect to '/login'
-    end
-  end
+ get '/tweets/new' do
+   if logged_in?
+     erb :'tweets/create_tweet'
+   else
+     redirect to '/login'
+   end
+ end
 
-  get '/tweets/:id' do
-      if logged_in?
-        @tweet = Tweet.find(params[:id])
-        erb :'tweets/show_tweet'
-      else
-        redirect '/login'
-      end
-    end
-
-  patch '/tweets/:id/edit' do
-    @tweet = Tweet.find_by(id: params[:id])
-    @user = current_user
-    if @tweet.update(content: params[:content], user: @user)
-      redirect to "/tweets/#{@tweet.id}"
-    else
-      redirect to "/tweets/#{@tweet.id}/edit"
-    end
-  end
-
-  delete '/tweets/:id/delete' do
-    @tweet = current_user.tweets.find_by(:id => params[:id])
-    if @tweet && @tweet.destroy
-      redirect "/tweets"
-    else
-      redirect "/tweets/#{@tweet.id}"
-    end
-  end
-
-end
+ post '/tweets' do
+   if logged_in?
+     if params[:content] == ""
