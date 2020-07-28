c299f32e85e473f088cbce216c2bc3f91f9e047e
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-28 13:38:27 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -46,3 +46,60 @@ class TweetsController < ApplicationController
    if logged_in?
      @tweet = Tweet.find_by_id(params[:id])
      if @tweet && @tweet.user == current_user
+       erb :'tweets/edit_tweet'
+    else
+      redirect to '/tweets'
+    end
+  else
+    redirect to '/login'
+  end
+end
+
+patch '/tweets/:id' do
+  if logged_in?
+    if params[:content] == ""
+      redirect to "/tweets/#{params[:id]}/edit"
+    else
+      @tweet = Tweet.find_by_id(params[:id])
+      if @tweet && @tweet.user == current_user
+        if @tweet.update(content: params[:content])
+          redirect to "/tweets/#{@tweet.id}"
+        else
+          redirect to "/tweets/#{@tweet.id}/edit"
+        end
+      else
+        redirect to '/tweets'
+      end
+    end
+  else
+    redirect to '/login'
+  end
+end
+
+delete '/tweets/:id/delete' do
+  if logged_in?
+    @tweet = Tweet.find_by_id(params[:id])
+    if @tweet && @tweet.user == current_user
+@weezwo
+weezwo on Jun 14, 2018
+Prefer to find tweets that are already only associated to the current_user than to manually check the user_id foreign key of the tweet against the current_user.id
+
+tweet = current_user.tweets.find_by(id: params[:id])
+if tweet && tweet.destroy
+redirect "/tweets"
+else
+redirect "/tweets/#{tweet.id}"
+end
+We're using the association collection method provided by has_many to query the user's tweets.
+
+Note the usage of find_by vs find. Had we used find here, ActiveRecord would raise an error in the event that we could not find a tweet associated with this user. find_by will allow this action to fail gracefully, simply returning nil if the user is trying to delete a tweet that they do not have access to.
+
+@DahliaBloomstone	Reply…
+      @tweet.delete
+    end
+    redirect to '/tweets'
+  else
+    redirect to '/login'
+  end
+end
+end
