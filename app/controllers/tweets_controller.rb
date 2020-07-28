253e70a8761fc71377291e3f4de2e9e1c0b5255f
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-28 13:30:12 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -42,15 +42,14 @@ class TweetsController < ApplicationController
     end
   end
 
-  get '/tweets/:id/edit' do
-    @tweet = Tweet.find_by(id: params[:id])
-
-    if logged_in? && @tweet.user == current_user
-      erb :"tweets/edit"
-    else
-      redirect to '/login'
+  get '/tweets/:id' do
+      if logged_in?
+        @tweet = Tweet.find(params[:id])
+        erb :'tweets/show_tweet'
+      else
+        redirect '/login'
+      end
     end
-  end
 
   patch '/tweets/:id/edit' do
     @tweet = Tweet.find_by(id: params[:id])
